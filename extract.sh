d95a203767327f707a347d53ec9acaed44a887c8
Simon Simonsson <simonsson.simon@gmail.com>
2018-08-17 00:37:26 +0200

    script for extracting data from recorded videos

---
+++ extract.sh
@@ -0,0 +1,45 @@
+export LD_LIBRARY_PATH=/usr/local/lib
+RECORDINGS=data/recordings
+BIN=./bin/extract
+
+
+function ex
+{
+	$BIN --image $1 --flip --homography --visualize
+}
+
+if [ "$#" -gt 0 ]
+then
+	ex $1
+	exit
+fi
+
+
+# loop over all recording directories
+for REC in $RECORDINGS/*
+do
+	echo $REC
+	# loop over all RGB frames
+	for FRAME in $REC/rgb/*.jpg
+	do
+		# extract data
+		# if we didn't find any tag we continue to the next frame
+		OUT=`$BIN --image $FRAME --flip`
+		if [ "$OUT" == "no tags detected" ]
+		then
+			continue
+		fi
+
+		IFS=':' read -ra TAG <<< "$OUT"
+		ID="${TAG[0]}"
+
+		echo "in frame $FRAME we found TAG#$ID"
+		if [ "$ID" == "12" ]
+		then
+			ex $FRAME
+			exit
+		fi
+	done
+done
+
+
