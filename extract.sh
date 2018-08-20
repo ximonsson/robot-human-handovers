54312b9081f47aedae8e958d01923fb60b52e971
Simon Simonsson <simonsson.simon@gmail.com>
2018-08-21 01:15:52 +0200

    better reading

--- extract.sh
+++ extract.sh
@@ -1,19 +1,19 @@
 export LD_LIBRARY_PATH=/usr/local/lib
 RECORDINGS=data/recordings
 BIN=./bin/extract
-
+OUTFILE=
 
 function ex
 {
 	$BIN --image $1 --flip
 }
 
+# if an input argument was supplied only run the extraction on that file
 if [ "$#" -gt 0 ]
 then
 	ex $1
 	exit
 fi
-
 
 # loop over all recording directories
 for REC in $RECORDINGS/*
@@ -24,16 +24,21 @@ do
 	do
 		# extract data
 		# if we didn't find any tag we continue to the next frame
-		OUT=`ex $FRAME`
+		OUT="$(ex $FRAME 2>&1)"
 		if [ $? -ne 0 ]
 		then
 			continue
 		fi
 
-		IFS=':' read -ra TAG <<< "$OUT"
-		ID="${TAG[0]}"
-		echo "in frame $FRAME we found TAG#$ID"
+		echo $FRAME
+		IFS=$'\n'
+		for LINE in $OUT
+		do
+			echo " > $LINE"
+		done
+
+		#IFS=':' read -ra TAG <<< "$OUT"
+		#ID="${TAG[0]}"
+		#echo "in frame $FRAME we found TAG#$ID"
 	done
 done
-
-
