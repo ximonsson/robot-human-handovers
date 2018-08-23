e50a1388b68301c2d9383abd2a13a99989612c7f
Simon Simonsson <simonsson.simon@gmail.com>
2018-08-23 22:44:31 +0200

    little easier to use

--- tagid.sh
+++ tagid.sh
@@ -1,14 +1,15 @@
-DATA=data
-OBJECTS=$DATA/objects
+DIR=$1
 BIN=bin/extract_tagid
 
-for IM in $OBJECTS/rgb/*.jpg
+echo "Looping over RGB images in $DIR/rgb ... "
+for IM in $DIR/rgb/*.jpg
 do
 	# extract data
 	# if we didn't find any tag we continue to the next frame
-	OUT="$($BIN --image $IM --flip 2>&1)"
+	OUT="$(LD_LIBRARY_PATH=/usr/local/lib $BIN --image $IM --flip 2>&1)"
 	if [ $? -ne 0 ]
 	then
+		echo "No tag detected in $IM"
 		continue
 	fi
 
