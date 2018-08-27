868e439df4dd4bae64cb4d4ccfaf2d7add20f293
Simon Simonsson <simonsson.simon@gmail.com>
2018-08-27 17:38:03 +0200

    rectifying ROI

--- src/rec/play.py
+++ src/rec/play.py
@@ -3,8 +3,10 @@ import sys
 import os
 import time
 
-ROI_W = 300
-ROI_H = 300
+ROI_W = 350
+ROI_H = 400
+ROI_X = -int(ROI_W / 2)
+ROI_Y = -100
 
 directory = sys.argv[1]
 files = os.listdir(directory)
@@ -15,8 +17,8 @@ for filename in files:
 	im = cv2.flip(cv2.imread(os.path.join(directory, filename)), 1)
 	cv2.rectangle(
 		im,
-		(int(im.shape[1]/2-ROI_W/2), int(im.shape[0]/2-ROI_H/2)),
-		(int(im.shape[1]/2+ROI_W/2), int(im.shape[0]/2+ROI_H/2)),
+		(int(im.shape[1]/2+ROI_X), int(im.shape[0]/2+ROI_Y)),
+		(int(im.shape[1]/2+ROI_X+ROI_W), int(im.shape[0]/2+ROI_Y+ROI_H)),
 		(0, 0, 255),
 		2)
 	cv2.imshow("opencv playback", im)
