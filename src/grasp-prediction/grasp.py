a7ba00a7e937a2ee6b8cd906427626c8d21af036
Simon Simonsson <simonsson.simon@gmail.com>
2018-09-12 22:19:01 +0200

    fix so it should maybe run

--- src/grasp-prediction/grasp.py
+++ src/grasp-prediction/grasp.py
@@ -10,7 +10,7 @@ batch_size = 128
 # network parameters
 dropout = 0.5
 outputs = 2
-train_layers = ["fc6", "fc7"]
+train_layers = ["fc6", "fc7", "fc8"]
 
 # tensorflow variables, x input, y output and dropout
 x = tf.placeholder(tf.float32, [batch_size, alexnet.IN_WIDTH, alexnet.IN_HEIGHT, alexnet.IN_DEPTH])
@@ -18,7 +18,7 @@ y = tf.placeholder(tf.float32, [None, outputs])
 keep_prob = tf.placeholder_with_default(1.0, shape=())
 
 # create network
-m = alexnet.model(x, keep_prob, outputs)
+m = alexnet.model(x, keep_prob, classes=outputs)
 
 # calculate loss
 with tf.name_scope("loss"):
@@ -33,10 +33,10 @@ correct_pred = tf.equal(tf.argmax(m, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 tf.summary.scalar("accuracy", accuracy)
 
-with tf.Session() as session:
+with tf.Session() as s:
     # initialize and load weights
     s.run(tf.global_variables_initializer())
-    alexnet.load_weights("weights/bvlc_alexnet.npy", session, train_layers)
+    alexnet.load_weights("data/weights/bvlc_alexnet.npy", s, train_layers)
 
     # train
-    tf.run(train_op, feed_dict={x: None, y: None, keep_prob:1.0-dropout})
+    s.run(train_op, feed_dict={x: None, y: None, keep_prob:1.0-dropout})
