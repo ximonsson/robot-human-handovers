24e1032c32c358b1762d8b7db27051e815fd8471
Simon Simonsson <simonsson.simon@gmail.com>
2018-10-09 00:21:38 +0200

    writing summary of training

--- src/classification/main.py
+++ src/classification/main.py
@@ -21,13 +21,13 @@ DATA_RATIO = 0.8
 
 # learning parameters
 learning_rate = 0.001
-epochs = 3
-batch_size = 10
+epochs = 10
+batch_size = 64
 
 # network parameters
 dropout = 0.5
 outputs = 2
-train_layers = ["fc8"]
+train_layers = ["fc6", "fc7", "fc8"]
 
 # tensorflow variables, x input, y output and dropout
 x = tf.placeholder(tf.float32, [batch_size, alexnet.IN_WIDTH, alexnet.IN_HEIGHT, alexnet.IN_DEPTH])
@@ -39,22 +39,22 @@ objects = [
 		#"ball",
 		"bottle",
 		"box",
-		"can",
 		"cutters",
 		"glass",
-		"hammer",
 		"knife",
 		"scissors",
-		"mug",
-		"pen",
-		"pitcher",
 		"brush",
 		"scalpel",
+		"can",
 		"screwdriver",
+		"pitcher",
+		"hammer",
+		"pen",
+		"cup",
 		#"tube",
 		]
 
-N = 11
+N = 12
 training_objects = objects[:N]
 test_objects = objects[N:]
 
@@ -76,32 +76,44 @@ with tf.name_scope("loss"):
 with tf.name_scope("train"):
 	# TODO gradient descent?
 	train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+tf.summary.scalar("cross_entropy", loss)
 
 # accuracy of the model
 with tf.name_scope("accuracy"):
 	correct_pred = tf.equal(tf.argmax(m, 1), tf.argmax(y, 1))
 	accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+tf.summary.scalar("accuracy", accuracy)
 
 with tf.name_scope("testing"):
 	classifier = tf.nn.softmax(m) # for testing output
 	test = tf.equal(tf.argmax(classifier, 1), tf.argmax(y, 1))
 	test_accuracy = tf.reduce_mean(tf.cast(test, tf.float32))
+tf.summary.scalar("test_accuracy", test_accuracy)
 
 
-tf.summary.scalar("accuracy", accuracy)
-tf.summary.scalar("cross_entropy", loss)
+merged_summary = tf.summary.merge_all()
+
 
 INPUT_DIMENSIONS = [alexnet.IN_WIDTH, alexnet.IN_HEIGHT, alexnet.IN_DEPTH]
 
 
 with tf.Session() as s:
+	# split dataset
+	training_data, validation_data = data.datasets(DATA, training_objects, DATA_RATIO)
+	test_data, _ = data.datasets(DATA, test_objects, 1.0)
+
+	print("Training on {} images, validating on {} images, and testing on {}".format(
+		len(training_data),
+		len(validation_data),
+		len(test_data)))
+
+	# create writers for summary
+	train_writer = tf.summary.FileWriter("results/classification/train", s.graph)
+	test_writer = tf.summary.FileWriter("results/classification/test")
+
 	# initialize and load weights
 	s.run(tf.global_variables_initializer())
 	alexnet.load_weights("data/weights/bvlc_alexnet.npy", s, train_layers)
-
-	# split dataset
-	training_data, validation_data = data.datasets(DATA, training_objects, DATA_RATIO, size=200)
-	test_data, _ = data.datasets(DATA, test_objects, 1.0, size=40)
 
 	# for each epoch fit the model to the training set and
 	# calculate accuracy over the validation set
@@ -111,31 +123,37 @@ with tf.Session() as s:
 
 		# train
 		print("Training...", end="", flush=True)
+		n_train_batches = int(len(training_data)/batch_size)
 		for batch, X, Y in data.batches(training_data, batch_size, INPUT_DIMENSIONS, outputs):
+			# run training operation and calculate loss after
 			s.run(train_op, feed_dict={x: X, y: Y, keep_prob: 1.0-dropout})
+			l = s.run(loss, feed_dict={x: X, y: Y, keep_prob: 1.0})
 			print(
-					"\r{:15} {}".format(
+					"\r{:15} {} => Loss {:.4f}".format(
 						"Training:",
-						progressbar(batch+1, int(len(training_data)/batch_size))),
+						progressbar(batch+1, n_train_batches),
+						l),
 					flush=True,
 					end="")
+			# summarize
+			summary = s.run(merged_summary, feed_dict={x: X, y: Y, keep_prob: 1.0})
+			train_writer.add_summary(summary, batch + epoch * n_train_batches))
 		print()
 
 		# validate
 		acc = 0
 		print("Validating...", end="", flush=True)
 		for batch, X, Y in data.batches(validation_data, batch_size, INPUT_DIMENSIONS, outputs):
-			b = batch+1
+			b = batch + 1
 			acc += s.run(accuracy, feed_dict={x: X, y: Y, keep_prob: 1.0})
-			acc /= b
 			print(
 					"\r{:15} {} => Accuracy {:.4f}%".format(
 						"Validating:",
 						progressbar(b, int(len(validation_data)/batch_size)),
-						acc*100),
+						(acc / b * 100)),
 					flush=True,
 					end="")
-		print("\n{} Validation Accuracy: {:.4f}%\n".format(datetime.now(), acc*100))
+		print("\n{} Validation Accuracy: {:.4f}%\n".format(datetime.now(), (acc / (batch + 1) * 100)))
 
 	# test accuracy
 	print("Testing...", end="", flush=True)
@@ -143,14 +161,13 @@ with tf.Session() as s:
 	for batch, X, Y in data.batches(test_data, batch_size, INPUT_DIMENSIONS, outputs):
 		b = batch+1
 		acc += s.run(test_accuracy, feed_dict={x: X, y: Y, keep_prob: 1.0})
-		acc /= b
 		print(
 				"\r{:15} {} => Accuracy {:.4f}%".format(
 					"Testing:",
 					progressbar(b, int(len(test_data)/batch_size)),
-					acc*100),
+					(acc/b)*100),
 				flush=True,
 				end="")
 	print()
-	print("{} Test Accuracy: {:.4f}%".format(datetime.now(), acc*100))
+	print("{} Test Accuracy: {:.4f}%".format(datetime.now(), (acc/b)*100))
 	print()
